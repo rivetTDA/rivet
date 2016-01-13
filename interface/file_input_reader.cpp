20fe45924396b72bf5aca7c1f8eb9834f31da753
Matthew Wright <mlwright@ima.umn.edu>
2016-01-13 15:26:59 -0600

    improvements to input file formats and corresponding code updates

--- interface/file_input_reader.cpp
+++ interface/file_input_reader.cpp
@@ -2,15 +2,16 @@
 
 #include <QChar>
 #include <QString>
+#include <QDebug>
 
 FileInputReader::FileInputReader(QFile& file) :
     in(&file),
     next_line_found(false)
 {
-    find_next();
+    find_next_line();
 }
 
-void FileInputReader::find_next()
+void FileInputReader::find_next_line()
 {
     QChar comment('#');
     while( !in.atEnd() && !next_line_found )
@@ -21,10 +22,11 @@ void FileInputReader::find_next()
         //else -- found a nonempty next line
         next_line_found = true;
         next_line_tokens = line.split(QRegExp("\\s+"));
+        next_list_position = 0;
     }
 }
 
-bool FileInputReader::has_next()
+bool FileInputReader::has_next_line()
 {
     return next_line_found;
 }
@@ -34,8 +36,23 @@ QStringList FileInputReader::next_line()   ///TODO: maybe too much copying of QS
     QStringList current = next_line_tokens;
 
     next_line_found = false;
-    find_next();
+    find_next_line();
 
     return current;
 }
 
+bool FileInputReader::has_next_token()
+{
+    if(next_list_position < next_line_tokens.size())    //then there is another token in the current line
+        return true;
+
+    //otherwise, see if there is another nonempty line
+    next_line_found = false;
+    find_next_line();
+    return next_line_found;
+}
+
+QString FileInputReader::next_token()
+{
+    return next_line_tokens.at(next_list_position++);
+}
--- interface/file_input_reader.h
+++ interface/file_input_reader.h
@@ -1,3 +1,9 @@
+/**
+ * \brief	Reads a file, ignoring white space and comments. Returns tokens by line or individually.
+ * \author	Matthew L. Wright
+ * \date	2015
+ */
+
 #ifndef FILEINPUTREADER_H
 #define FILEINPUTREADER_H
 
@@ -8,15 +14,20 @@
 class FileInputReader
 {
     public:
-        FileInputReader(QFile& file);
-        bool has_next();
-        QStringList next_line();
+        FileInputReader(QFile& file);   //constructor
+
+        bool has_next_line();       //true iff the file has another line of printable, non-commented characters
+        QStringList next_line();    //returns the next line, as a QStringList
+
+        bool has_next_token();      //true iff the file has more printable, non-commented characters -- caution, this might advance to the next line!
+        QString next_token();       //returns the next token (advancing to the next line if necessary)
 
     private:
         QTextStream in;
         bool next_line_found;
         QStringList next_line_tokens;
-        void find_next();
+        void find_next_line();
+        int next_list_position;
 };
 
 #endif // FILEINPUTREADER_H
