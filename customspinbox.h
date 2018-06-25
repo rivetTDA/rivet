b9a2c359ee8caf94fbb9aa3d9a4ad4ec12364c8c
Simon Segert <simonsegert@gmail.com>
2018-06-25 16:08:48 -0400

    further tweak to number fields

--- customspinbox.h
+++ customspinbox.h
@@ -13,7 +13,20 @@ public:
     //this function controls the appearance of the text
     QString textFromValue(double val) const {
 
-        QString str = QString::number( val, 'f', decimals() );
+
+        QString str = QString::number(fabs(val), 'f', decimals() );
+        //take absolute value so that "-0" is not immediately corrected to "0"
+
+        //make sure there is not a zero before the decimal point for numbers in (-1,1)
+        if((val>-1&& val<0) ||(val>0&& val<1)){
+           str.replace("0.",".");
+        }
+
+        //add in the minus sign if the value is negative, or if the user was in the middle of typing
+        //a negative value >-1 (e.g. -0.5)
+        if(val<0 ||(lineEdit()->text().size()>0&& lineEdit()->text().at(0)=="-")){
+            str="-"+str;
+        }
 
         str.remove( QRegExp("0+$") ); // Remove any number of trailing 0's
         str.remove( QRegExp("\\.$") ); // If the last character is just a '.' then remove it
