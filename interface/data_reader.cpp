af013219e3eec6308f4f213c989d5afde4bfca55
mlesnick <mlesnick@users.noreply.github.com>
2023-07-25 01:15:15 -0400

    Remove extra sorting step.

--- interface/data_reader.cpp
+++ interface/data_reader.cpp
@@ -602,11 +602,7 @@ FileContent DataReader::read_bifiltration(std::ifstream& stream, Progress& progr
             gradesOfApp.push_back(Grade(x_indexes[current_grade], y_indexes[current_grade]));
             current_grade++;
         }
-
-        //TODO: Double-check that this sorting hasn't been done earlier.
-        //Mike: I reorganized the code slightly so that the arguments of add_simplex are const references, which seems better, but that means any sorting should happen before.
-        std::sort(gradesOfApp.begin(), gradesOfApp.end());
-        std::sort(it->first.begin(), it->first.end());
+        
         data->bifiltration_data->update_grades(gradesOfApp);
         data->bifiltration_data->add_simplex(it->first, gradesOfApp);
     }
